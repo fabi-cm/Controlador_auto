49ac4565bd26fa627a60f6abf9e247784bcf8668
Fabio Copa Mejia <dahofcmmejia@gmail.com>
2023-04-09 17:26:45 -0400

    TDD El pibe puede ingresar la posición inicial del auto en formato matriz/X,YD donde D es la orientación

--- src/mover_auto.js
+++ src/mover_auto.js
@@ -1,9 +1,35 @@
-function mover_auto(cadena){
-    if (cadena === "" || cadena === " "){
+function mover_auto(comandos){
+    let datos = comandos.split("/")
+    let matriz;
+    let posicion;
+    let direccion;
+    
+    if (comandos === "" || comandos === " "){
         return "Cadena vacia";
     }
-    let matriz = cadena.split(",")
-    return toString(matriz);
+    else if (datos.length < 2){
+        return `${matriz}`;
+    }
+    
+    matriz = extraer_matriz(datos[0])
+    posicion = extraer_posicion(datos[1])
+    direccion = extraer_direccion(datos[1])
+    
+    return `${matriz},${posicion} ${direccion}`;
+}
+
+function extraer_matriz(datos){
+    let matriz = datos.split(",").map(Number)
+    return matriz
+}
+function extraer_posicion(datos){
+    let posicion = datos.split(",");
+    return posicion
+}
+
+function extraer_direccion(datos){
+    let direccion = datos;
+    return direccion
 }
 
 export default mover_auto;
--- src/mover_auto.spec.js
+++ src/mover_auto.spec.js
@@ -1,10 +1,35 @@
-import mover_auto from "./mover_auto"
+import mover_auto from "./mover_auto";
 
-describe('Mover un auto', () => {
-    it('Al tener una cadena vacia, Retorna -> cadena vacia', () =>{
-        expect(mover_auto("").toString("Cadena vacia"))
-    })
-    it('El conductor puede ingresar las dimensiones del área de juego en formato "X, Y": "1,3" -> [1,3]', () => {
-        expect(mover_auto("1,3").toString("[1,3]"))
-    })
-})
+describe("Mover un auto", () => {
+  it("Al tener una cadena vacia, Retorna -> cadena vacia", () => {
+    expect(mover_auto("").toString("Cadena vacia"));
+  });
+  it('El pibe puede ingresar las dimensiones del área de juego en formato "X, Y": "5,5" -> [5,5]', () => {
+    expect(mover_auto("5,5").toString("{5,5}"));
+  });
+  
+  it('El pibe puede ingresar la posición inicial del auto en formato "matriz/X, Y, D", donde "D" es la orientación', () => {
+    expect(mover_auto("5,5/1,2N").toString("{5,5}, {1,2 N}"));
+  });
+  xit('El pibe puede ingresar los comandos para el auto como una cadena de caracteres que consiste en las letras "I" (Izquierda), "D" (Derecha) y "A" (Avanzar)', () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit('La aplicación debe devolver la posición final del auto después de ejecutar los comandos en formato "X, Y, D"', () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit("La aplicación debe validar que la dimensión del área de juego sea mayor a cero en ambas dimensiones.", () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit("La aplicación debe validar que la posición inicial del auto se encuentre dentro del área de juego.", () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit('La aplicación debe validar que los comandos ingresados para cada auto solo contengan las letras "I", "D" y "A"', () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit("Si se ingresa un comando inválido, la aplicación debe mostrar un mensaje de error indicando el problema", () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+  xit("La posición inicial y final del auto deben ser claramente visibles en la interfaz gráfica", () => {
+    expect(mover_auto("1,3").toString("[1,3]"));
+  });
+});
